f5e8feb0d3afda591910ee45855e80831943a5da
hemsingh780 <hemdsinghdhoni_bca18_21@its.edu.in>
2022-05-20 16:59:59 +0530

    new files upload

---
+++ frontend/src/components/header/Header.js
@@ -0,0 +1,13 @@
+import React from 'react';
+ import { Container } from './HeaderStyle';
+
+const Header = () => {
+  return (
+    <Container>
+                hello 
+    </Container>
+
+  )
+}
+
+export default Header
