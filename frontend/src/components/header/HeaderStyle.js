af691fde8e9eece895d1f15037305c30a19e6478
hemsingh780 <hemdsinghdhoni_bca18_21@its.edu.in>
2022-05-20 22:45:16 +0530

    add files to the repo

--- frontend/src/components/header/HeaderStyle.js
+++ frontend/src/components/header/HeaderStyle.js
@@ -2,6 +2,60 @@ import styled from "styled-components";
 
 
 export const Container = styled.div`
-   background:blue;
+   display:flex;
+   padding: 12px;
+   box-shadow: 0px 1px 0px 0px #b8b2b2;
    
+`
+export const HouseButton =  styled.div`
+      border: 1px solid;
+      width: 119px;
+      display: flex;
+      justify-content: center;
+      padding: 4px 0;
+      font-weight:bold;
+      cursor:pointer;
+      &:hover{
+         background:blue;
+         color:white
+      }
+`
+
+export const First = styled.div`
+      flex:1;
+   
+`
+export const HamAndImg = styled.div`
+      display: flex;
+      box-shadow: 1px 0px 2px 1px #767272; 
+      padding: 2px;
+      border-radius: 13px;
+      width: 68px;
+      justify-content: space-around;
+`
+export const Second = styled.div`
+   flex:1;
+   display:flex;
+   justify-content:space-around;
+   
+`
+export const IconHam = styled.span`
+         display: flex;
+         justify-content: center;
+         align-items: center;
+         cursor:pointer;
+`
+export const ImageSpan = styled.span`
+`
+export const Image = styled.img`
+         object-fit: cover;
+         height: 24px;
+         width: 24px;
+`
+export const IconStyle = styled.div`
+    cursor:pointer;
+`
+export const NavLink = styled.div`
+
+
 `
